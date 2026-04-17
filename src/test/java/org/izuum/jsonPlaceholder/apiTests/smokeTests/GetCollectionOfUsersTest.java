5279908b262f5ce2b2f2570f62c86ad6a4852b3f
Roman <dstep.ds@gmail.com>
2026-04-17 15:47:07 +0300

    закончил smoke-тесты

---
+++ src/test/java/org/izuum/jsonPlaceholder/apiTests/smokeTests/GetCollectionOfUsersTest.java
@@ -0,0 +1,55 @@
+package org.izuum.jsonPlaceholder.apiTests.smokeTests;
+
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.izuum.jsonPlaceholder.specification.Specification.requestSpecification;
+
+@DisplayName("TC-011: Получение коллекции users")
+public class GetCollectionOfUsersTest {
+
+    @Test
+    @DisplayName("TC-011: Статус-код 200")
+    public void getRequestCheckStatusCode(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @DisplayName("TC-011: Ответ является массивом")
+    public void getRequestCheckResponseIsAnArray(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users")
+                .then()
+                .body("$", instanceOf(List.class));
+    }
+
+    @Test
+    @DisplayName("TC-011: Элементы содержат поля id, name, username, email, address, phone, website и company")
+    public void getRequestCheckElementsHasFields(){
+        RestAssured.given()
+                .when()
+                .get("/users")
+                .then()
+                .body("every {it.containsKey('id)}", is(true))
+                .body("every {it.containsKey('name')}", is(true))
+                .body("every {it.containsKey('username')}", is(true))
+                .body("every {it.containsKey('email')}", is(true))
+                .body("every {it.containsKey('address')}", is(true))
+                .body("every {it.containsKey('phone')}", is(true))
+                .body("every {it.containsKey('website')}", is(true))
+                .body("every {it.containsKey('company')}", is(true));
+    }
+}
---
+++ src/test/java/org/izuum/jsonPlaceholder/apiTests/smokeTests/GetUserByIdTest.java
@@ -0,0 +1,50 @@
+package org.izuum.jsonPlaceholder.apiTests.smokeTests;
+
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.izuum.jsonPlaceholder.specification.Specification.requestSpecification;
+
+@DisplayName("TC-012: Получение user по id")
+public class GetUserByIdTest {
+
+    @Test
+    @DisplayName("TC-012: Статус-код 200")
+    public void getRequestCheckStatusCode(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users/1")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @DisplayName("TC-012: Ответ является объектом")
+    public void getRequestResponseIsAnObject(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users/1")
+                .then()
+                .body("$", instanceOf(Object.class));
+    }
+
+    @Test
+    @DisplayName("TC-012: Вложенные поля address и company присутствуют")
+    public void getRequestCheckThatObjectHasNestedFields(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users/1")
+                .then()
+                .body("", hasKey("address"))
+                .body("", hasKey("company"))
+                .body("address", instanceOf(Object.class))
+                .body("company", instanceOf(Object.class));
+    }
+}
