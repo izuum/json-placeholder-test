0c80cbe4fc8789d2a7d1f728b9bd2e657ae63917
Roman Shaposhnikov <dstep.ds@gmail.com>
2026-04-23 14:35:18 +0300

    добавил allure для генерации отчетов о тестировании

--- src/test/java/org/izuum/jsonPlaceholder/specification/Specification.java
+++ src/test/java/org/izuum/jsonPlaceholder/specification/Specification.java
@@ -1,11 +1,16 @@
 package org.izuum.jsonPlaceholder.specification;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class Specification {
-
+    static {
+        RestAssured.filters(new AllureRestAssured());
+        System.out.println("Allure запущен");
+    }
     public static RequestSpecification requestSpecification(){
         return new RequestSpecBuilder()
                 .setBaseUri("https://jsonplaceholder.typicode.com")
