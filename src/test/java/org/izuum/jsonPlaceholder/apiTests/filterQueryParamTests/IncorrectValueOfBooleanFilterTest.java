5e8cfa115c66fb327e8e7bf0be163431853199ad
Roman <dstep.ds@gmail.com>
2026-05-05 13:07:18 +0300

    дописал тесты фильтрации query-параметрами

--- src/test/java/org/izuum/jsonPlaceholder/apiTests/filterQueryParamTests/IncorrectValueOfBooleanFilterTest.java
+++ src/test/java/org/izuum/jsonPlaceholder/apiTests/filterQueryParamTests/IncorrectValueOfBooleanFilterTest.java
@@ -2,31 +2,49 @@ package org.izuum.jsonPlaceholder.apiTests.filterQueryParamTests;
 
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.izuum.jsonPlaceholder.apiTestUtils.ApiTestUtils.getResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("TC-036: Некорректное значение boolean-фильтра")
 public class IncorrectValueOfBooleanFilterTest{
 
     @Test
-    @DisplayName("TC-036: Проверка что ответ стабилен")
+    @DisplayName("TC-036: Проверка стабильности ответа")
     public void getRequestCheckStatusCodeMultipleRequest(){
         Response responseOne = getResponse("/todos?completed=invalid");
         Response responseTwo = getResponse("/todos?completed=invalid");
         Response responseThree = getResponse("/todos?completed=invalid");
 
-        Assertions.assertEquals(HttpStatus.SC_OK, responseOne.getStatusCode());
-        Assertions.assertEquals(responseOne.getStatusCode(), responseTwo.getStatusCode());
-        Assertions.assertEquals(responseTwo.getStatusCode(), responseThree.getStatusCode());
+        assertEquals(HttpStatus.SC_OK, responseOne.getStatusCode());
+        assertEquals(responseOne.getStatusCode(), responseTwo.getStatusCode());
+        assertEquals(responseTwo.getStatusCode(), responseThree.getStatusCode());
 
-        Assertions.assertEquals(0, responseOne.jsonPath().getList("$").size());
-        Assertions.assertEquals(responseOne.jsonPath().getList("$").size(),
+        assertEquals(0, responseOne.jsonPath().getList("$").size());
+        assertEquals(responseOne.jsonPath().getList("$").size(),
                 responseTwo.jsonPath().getList("$").size());
-        Assertions.assertEquals(responseTwo.jsonPath().getList("$").size(),
+        assertEquals(responseTwo.jsonPath().getList("$").size(),
                 responseThree.jsonPath().getList("$").size());
     }
 
+    @Test
+    @DisplayName("TC-036: Сервис не падает")
+    public void checkThatServiceNotFall(){
+        Response response = getResponse("/todos?completed=invalid");
+
+        assertTrue(response.statusCode() < 500);
+    }
+
+    @Test
+    @DisplayName("TC-036: Ожидается пустой массив или игнорирование фильтра в ожидаемом поведении")
+    public void checkThatResponseIsAnEmptyArray(){
+        Response response = getResponse("/todos?completed=invalid");
+
+        int size = response.jsonPath().getList("$").size();
+
+        assertEquals(0, size);
+    }
 }
