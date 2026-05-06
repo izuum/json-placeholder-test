6141aa4f31dbfee6b500ec4594acd24713d72b29
Roman <dstep.ds@gmail.com>
2026-05-06 16:57:59 +0300

    написал тесты связи между сущностями

---
+++ src/test/java/org/izuum/jsonPlaceholder/apiTests/nestedRoutesTests/GetPostsOfUserTest.java
@@ -0,0 +1,44 @@
+package org.izuum.jsonPlaceholder.apiTests.nestedRoutesTests;
+
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.izuum.jsonPlaceholder.apiTestUtils.ApiTestUtils.checkStatusCode;
+import static org.izuum.jsonPlaceholder.specification.Specification.requestSpecification;
+
+@DisplayName("TC-043: Получить posts пользователя")
+public class GetPostsOfUserTest {
+
+    @Test
+    @DisplayName("TC-043: Статус-код 200")
+    public void getRequestCheckStatusCode(){
+        checkStatusCode("/users/1/posts", HttpStatus.SC_OK);
+    }
+
+    @Test
+    @DisplayName("TC-043: Ответ является массивом")
+    public void getRequestCheckResponseIsAnArray(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users/1/posts")
+                .then()
+                .body("$", instanceOf(List.class));
+    }
+
+    @Test
+    @DisplayName("TC-043: Каждый элемент имеет userId=1")
+    public void checkThatEveryElementsHasUserIdIsOne(){
+        RestAssured.given()
+                .spec(requestSpecification())
+                .when()
+                .get("/users/1/posts")
+                .then()
+                .body("userId", everyItem(equalTo(1)));
+    }
+}
