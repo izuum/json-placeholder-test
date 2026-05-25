fa16e0f2ab75ee3c618b57e1db3f69febf254f35
Roman Shaposhnikov <dstep.ds@gmail.com>
2026-05-25 23:37:46 +0300

    smoke-тесты с dto, добавил dto

--- src/test/java/org/izuum/jsonPlaceholder/apiTestsWithDto/smokeTests/GetCollectionOfPostsWithDtoTest.java
+++ src/test/java/org/izuum/jsonPlaceholder/apiTestsWithDto/smokeTests/GetCollectionOfPostsWithDtoTest.java
@@ -31,10 +31,12 @@ public class GetCollectionOfPostsWithDtoTest {
         assertTrue(posts.size() >= 1);
 
         Post firstPost = posts.get(0);
-        assertNotNull(firstPost);
-        assertTrue(firstPost.getId() > 0);
-        assertNotNull(firstPost.getTitle());
-        assertNotNull(firstPost.getBody());
-        assertTrue(firstPost.getUserId() > 0);
+        assertNotNull(firstPost, "post равен null");
+        assertAll(
+                () -> assertTrue(firstPost.getId() > 0, "Id меньше 1"),
+                () -> assertNotNull(firstPost.getTitle(), "Title равен null"),
+                () -> assertNotNull(firstPost.getBody(), "Body равен null"),
+                () -> assertTrue(firstPost.getUserId() > 0, "UserId меньше 1")
+        );
     }
 }
--- src/test/java/org/izuum/jsonPlaceholder/apiTestsWithDto/smokeTests/GetPostByIdWithDtoTest.java
+++ src/test/java/org/izuum/jsonPlaceholder/apiTestsWithDto/smokeTests/GetPostByIdWithDtoTest.java
@@ -1,11 +1,12 @@
 package org.izuum.jsonPlaceholder.apiTestsWithDto.smokeTests;
 
-import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.izuum.jsonPlaceholder.apiTestsWithDto.dto.Post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.izuum.jsonPlaceholder.specification.Specification.requestSpecification;
+import static org.izuum.jsonPlaceholder.apiTestUtils.ApiTestUtils.getResponse;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("TC-002: Получение конкретного post по id")
@@ -14,19 +15,20 @@ public class GetPostByIdWithDtoTest {
     @Test
     @DisplayName("TC-002: Проверка, что статус-код 200, является объектом, id=1, обязательные поля присутствуют")
     public void getPostByIdWithDto(){
-        Post post = RestAssured.given()
-                .spec(requestSpecification())
-                .when()
-                .get("/posts/1")
-                .then()
-                .statusCode(200)
-                .extract()
-                .as(Post.class);
+        Response response = getResponse("/posts/1");
 
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+
+        String body = response.getBody().asString();
+        assertTrue(body.startsWith("{"), "Ответ должен быть объектом");
+
+        Post post = response.as(Post.class);
         assertNotNull(post);
-        assertEquals(1, post.getId());
-        assertNotNull(post.getTitle());
-        assertNotNull(post.getBody());
-        assertTrue(post.getUserId() > 0);
+        assertAll(
+                () -> assertEquals(1, post.getId(), "Id не равен 1"),
+                () -> assertNotNull(post.getTitle(), "Title равен null"),
+                () -> assertNotNull(post.getBody(), "Body равне null"),
+                () -> assertTrue(post.getUserId() > 0, "UserId меньше 1")
+        );
     }
 }
